731a1a0b8b0a5db07a7f6b5fa3456194f68af70a
nikhil sulegaon <nikhilsulegaon@gmail.com>
2019-08-05 18:28:54 -0600

    Create appropriate classes for DataGeneration and changing gitignore file.

--- constants.py
+++ constants.py
@@ -6,3 +6,8 @@ class Constants:
         LABEL = 'Truth Value'
 
     PATH_TO_DATASET = '/Users/nikhilsulegaon/Downloads/BLT-C_Boulder_Lies_and_Truths_Corpus.csv'
+
+    @staticmethod
+    def label_value(label):
+        label_to_num = {'T': 1, 'F': 2, 'D': 3}
+        return label_to_num[label.upper()]
--- data_cleanup.py
+++ data_cleanup.py
@@ -7,44 +7,40 @@ from constants import Constants
 Cols = Constants.Cols
 
 
-def remove_duplicates(data_frame):
-    data_frame.drop_duplicates(subset=Cols.ID, keep=False, inplace=True)
+class DatasetGenerator(object):
 
+    @staticmethod
+    def __remove_duplicates(data_frame):
+        data_frame.drop_duplicates(subset=Cols.ID, keep=False, inplace=True)
 
-def compute_and_add_metadata_to(data_frame):
-    pass
+    def __compute_and_add_metadata_to(self, data_frame):
+        pass
 
+    @staticmethod
+    def __filter_rows_by_type_of_labels(data_frame):
+        return data_frame[~data_frame[Cols.ID].str.startswith('[')]
 
-def filter_rows_by_type_of_labels(data_frame):
-    return data_frame[~data_frame[Cols.ID].str.startswith('[')]
+    @staticmethod
+    def __select_necessary_columns(data_frame):
+        return data_frame[[Cols.REVIEW, Cols.SENTIMENT, Cols.LABEL]]
 
+    @staticmethod
+    def __transform_values_of_sentiment_columns(data_frame):
+        data_frame[Cols.SENTIMENT] = np.where(data_frame[Cols.SENTIMENT] == 'pos', 1, 0)
 
-def select_necessary_columns(data_frame):
-    return data_frame[[Cols.REVIEW, Cols.SENTIMENT, Cols.LABEL]]
+    @staticmethod
+    def __transform_values_of_label(data_frame, treat_F_as_deceptive):
+        if treat_F_as_deceptive:
+            data_frame[Cols.LABEL] = np.where(data_frame[Cols.LABEL] == 'T', 1, 0)
+        else:
+            data_frame[Cols.LABEL] = data_frame[Cols.LABEL].map(Constants.label_value)
 
-
-def transform_values_of_sentiment_columns(data_frame):
-    data_frame[Cols.SENTIMENT] = np.where(dataset[Cols.SENTIMENT] == 'pos', 1, 0)
-
-
-def __get_label_value(label):
-    label_to_num = {'T': 1, 'F': 2, 'D': 3}
-    return label_to_num[label.upper()]
-
-
-def transform_values_of_label(data_frame, treat_F_as_deceptive):
-    if treat_F_as_deceptive:
-        data_frame[Cols.LABEL] = np.where(dataset[Cols.LABEL] == 'T', 1, 0)
-    else:
-        data_frame[Cols.LABEL] = data_frame[Cols.LABEL].map(__get_label_value)
-
-
-if __name__ == '__main__':
-    dataset = pd.read_csv(Constants.PATH_TO_DATASET)
-    remove_duplicates(dataset)
-    compute_and_add_metadata_to(dataset)
-    dataset = filter_rows_by_type_of_labels(dataset)
-    dataset = select_necessary_columns(dataset)
-    transform_values_of_sentiment_columns(dataset)
-    transform_values_of_label(dataset, treat_F_as_deceptive=False)
-    print(dataset.head())
+    def generate(self):
+        dataset = pd.read_csv(Constants.PATH_TO_DATASET)
+        self.__remove_duplicates(dataset)
+        self.__compute_and_add_metadata_to(dataset)
+        dataset = self.__filter_rows_by_type_of_labels(dataset)
+        dataset = self.__select_necessary_columns(dataset)
+        self.__transform_values_of_sentiment_columns(dataset)
+        self.__transform_values_of_label(dataset, treat_F_as_deceptive=False)
+        return dataset
